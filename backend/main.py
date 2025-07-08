49e07694b2b0b04545c23a70a394a82438f50d40
Yashbanna009 <rathorey083@gmail.com>
2025-07-08 23:48:35 +0530

    reverting hostng changes

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-# from fastapi.staticfiles import StaticFiles
 
 from backend.services.content_service import ContentService
 from backend.services.llm_service import LLMService
